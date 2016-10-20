137c76c16008ad5606f28d9ddadb4b9c82707bf0
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-10-20 15:14:29 +0900

    login.js での不要なバリデーションを削除

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -2,12 +2,6 @@ function isNoLogin() {
   return parseUri(location).queryKey.msg == "nologin";
 }
 var loginFunc = function() {
-  for (var i = 0; i < $('input').size(); i++) {
-    if (!$('input')[i].checkValidity()) {
-      $('#submit-for-validation').trigger("click");
-      return;
-    }
-  }
   var checkpointGroupId = parseUri(location).anchor;
   var userId = $('#user-id').val();
   var groupId = $('#group-id').val();
