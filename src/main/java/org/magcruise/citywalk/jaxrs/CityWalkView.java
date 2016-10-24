0126dc8bbfdefd6690707bd11e72517020345cea
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-10-24 15:35:13 +0900

    tutorialの追加

--- src/main/java/org/magcruise/citywalk/jaxrs/CityWalkView.java
+++ src/main/java/org/magcruise/citywalk/jaxrs/CityWalkView.java
@@ -47,7 +47,7 @@ public class CityWalkView extends JaxrsView {
 	private boolean isUnneededLogin(String filePathFromViewRoot) {
 		for (String unneededLoginPage : Arrays.asList("index.html", "clear.html", "dev.html",
 				"login.html", "signup.html", "how-to-use.html", "intro.html", "help.html",
-				"quick-start.html")) {
+				"tutorial.html")) {
 			if (filePathFromViewRoot.contains(unneededLoginPage)) {
 				return true;
 			}
