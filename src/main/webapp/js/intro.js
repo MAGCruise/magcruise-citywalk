1bf53be10c42bc17f472453f6fed97d7afd650f4
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-10-25 14:56:03 +0900

    intro.jsの切り出し

---
+++ src/main/webapp/js/intro.js
@@ -0,0 +1,25 @@
+$(function() {
+  $("#mag-nav").hide();
+  checkDevice();
+});
+
+function checkDevice() {
+  var uaParser = new UAParser();
+  var errMsg = "OSまたはWebブラウザが推奨環境ではないため，正しく動作しない可能性があります．";
+  if (uaParser.getOS().name === "Windows") {
+    if (uaParser.getBrowser().name !== "Chrome") {
+      alert(errMsg);
+    }
+  } else if (uaParser.getOS().name === "iOS") {
+    if (uaParser.getBrowser().name !== "Chrome"
+            && uaParser.getBrowser().name.indexOf("Safari") == -1) {
+      alert(errMsg);
+    }
+  } else if (uaParser.getOS().name === "Android") {
+    if (uaParser.getBrowser().name !== "Chrome") {
+      alert(errMsg);
+    }
+  } else {
+    alert(errMsg);
+  }
+}
