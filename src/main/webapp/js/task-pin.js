51e4279721acf086967f8c086b477b1411f9b61e
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-10-26 14:16:17 +0900

    #188 PINタスクで予測変換入力をしたときに回答ボタンがオンにならないを修正した

--- src/main/webapp/js/task-pin.js
+++ src/main/webapp/js/task-pin.js
@@ -12,37 +12,20 @@ $(function() {
 
   $('#label').text(task.label);
 
-  var answerSel = "#answer-text";
-  var buttonSel = "#btn-next";
-  // 回答の変更を監視
-  function checkChange(e) {
-    var old = v = $(e).find(answerSel).val();
-    return function() {
-      v = $(e).find(answerSel).val();
-      if (old != v) {
-        old = v;
-        $(buttonSel).prop("disabled", (v.length == 0));
-      }
-    }
-  }
-  $(answerSel).keyup(checkChange(this));
-
-  $(buttonSel).on('click',function() {
-    var text = $(answerSel).val().replace(/\s+/g, "").replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
+  $("#btn-next").on('click', function() {
+    var text = $("#answer-text").val().replace(/\s+/g, "").replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
       return String.fromCharCode(s.charCodeAt(0) - 0xFEE0);
     });
     if (!/^[0-9]{4}$/g.test(text)) {
       alert("半角数字4桁が入力されていません");
       return;
     }
-
-    addAnswerDic(checkpoint, task, text);
-
     var isCorrect = false;
     task.answerTexts.forEach(function(answerText) {
       isCorrect = isCorrect || (answerText == text);
     });
     if (isCorrect) {
+      addAnswerDic(checkpoint, task, text);
       addActivity(task, text, isCorrect);
     } else {
       alert("合い言葉が間違っています．");
