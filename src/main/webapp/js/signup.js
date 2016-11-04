9f10cd463c5bdbc135e2f2246616cbb3c987f918
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-11-05 08:44:53 +0900

    古いブラウザでcheckValidityに対応していないものに対応

--- src/main/webapp/js/signup.js
+++ src/main/webapp/js/signup.js
@@ -17,15 +17,22 @@ var registerFunc = function() {
     }
   });
 
+  var checkpointGroupId = parseUri(location).anchor;
+  var userId = $('#user-id').val().trim();
+  var groupId = $('#group-id').val().trim();
+
+  if(userId.length<1 || 8<userId.length){
+    swalAlert('サインアップ失敗',"1文字以上8文字以下で入力して下さい");
+    return;
+  }
+
   for (var i = 0; i < $('input').size(); i++) {
     if (!$('input')[i].checkValidity()) {
       $('#submit-for-validation').trigger("click");
       return;
     }
   }
-  var checkpointGroupId = parseUri(location).anchor;
-  var userId = $('#user-id').val();
-  var groupId = $('#group-id').val();
+
   new JsonRpcClient(new JsonRpcRequest(getBaseUrl(), "register", [userId, groupId,
       MAX_LENGTH_OF_USER_ID], function(data) {
     if (data.result) {
@@ -57,7 +64,8 @@ $(function() {
   $("form input").on('keypress', function(ev) {
     if ((ev.which && ev.which === 13) || (ev.keyCode && ev.keyCode === 13)) {
       ev.preventDefault();
-      registerFunc();
+      //registerFunc();
+      return;
     }
   });
 });
