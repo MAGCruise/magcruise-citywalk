24471184c1347243e1e76537156406ecf8ebf965
Yuu NAKAJIMA <yuu.nkjm.on@gmail.com>
2016-11-07 15:53:02 +0900

    説明ページの文言を調整

--- src/main/webapp/js/intro.js
+++ src/main/webapp/js/intro.js
@@ -2,7 +2,7 @@ $(function() {
   if (!getUserId() || !getCheckpointGroupId()) {
     $("#mag-nav").remove();
   }
-  $("#nav-title-wrapper").empty();
+  $("#nav-title").text("はじめに");
 
   $("#btn-join").on('click', function() {
     swalAlert("「歩きスマホ」はやめましょう", "画面を見つめながらの歩行は危険です", "warning", function() {
