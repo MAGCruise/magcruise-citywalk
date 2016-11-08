82c09a3aca2385a81b85e3c58b3aca0dc2ce1d34
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-11-08 12:41:41 +0900

    動作環境チェック回りの文言の修正

--- src/main/webapp/js/check-environment.js
+++ src/main/webapp/js/check-environment.js
@@ -1,4 +1,5 @@
 $(function() {
+
   if (!getUserId() || !getCheckpointGroupId()) {
     $("#nav-menu-wrapper").remove();
   }
@@ -12,14 +13,14 @@ $(function() {
   if (isEnableLocalStorage()) {
     $("#localStorage").html(
             $('<div class="alert alert-success">').html(
-                    '<span class="label label-success">SUCCESS</span> ' + "ローカルストレージが利用できます．"));
+                    '<span class="label label-success">OK</span> ' + "ブラウザ設定が正常です．"));
   } else {
     $("#localStorage")
             .html(
                     $('<div class="alert alert-danger">')
                             .html(
                                     '<span class="label label-danger">ERROR</span> '
-                                            + 'ローカルストレージが利用できません．ブラウザがプライベートモードになっているならばオフにして，ページを再読み込みして下さい．'
+                                            + 'ローカルストレージを利用できません．ブラウザがプライベートモードになっているならば，オフにして下さい．'
                                             + '<a class="alert-link" href="https://support.apple.com/ja-jp/HT203036">プライベートブラウズをオフにする  - Apple サポート<i class="fa fa-external-link"></i></a> を見る'));
     $("#btn-join").prop("disabled", true);
   }
@@ -48,11 +49,11 @@ function onHeadingChange(event) {
     $("#compass").html(
             $('<div class="alert alert-warning">').html(
                     '<span class="label label-warning">WARN</span> '
-                            + "電子コンパスを利用できません．コンパスによるナビゲーションはできません．"));
+                            + "電子コンパスを利用できないため，コンパスによるナビゲーションはできません．"));
   } else {
     $("#compass").html(
             $('<div class="alert alert-success">').html(
-                    '<span class="label label-success">SUCCESS</span> ' + "電子コンパスを利用できます．"));
+                    '<span class="label label-success">OK</span> ' + "電子コンパスを利用できます．"));
   }
 
 }
@@ -63,7 +64,7 @@ function checkGPS() {
                   function(pos) {
                     $("#gps").html(
                             $('<div class="alert alert-success">').html(
-                                    '<span class="label label-success">SUCCESS</span> '
+                                    '<span class="label label-success">OK</span> '
                                             + "位置情報サービスを利用できます．"));
                   },
                   function(error) {
@@ -72,8 +73,9 @@ function checkGPS() {
                                     $('<div class="alert alert-warning">')
                                             .html(
                                                     '<span class="label label-warning">WARN</span> '
-                                                            + "位置情報サービスを利用できません．残り距離，コンパスによるナビゲーションなどは利用できません．"
-                                                            + '<i class="glyphicon glyphicon-hand-right"></i> <a class="alert-link" href="https://wasenavi.magcruise.org/magcruise-citywalk/app/troubleshooting.html#gps-settings">位置情報サービスの設定</a> を見る．'));
+                                                            + "位置情報サービスが利用できないため，残り距離，コンパスによるナビゲーションができません．"
+                                                            + '<i class="glyphicon glyphicon-hand-right"></i> '
+                                                            + '<a class="alert-link" href="troubleshooting.html#gps-settings">位置情報サービスの設定</a> を見る．'));
                   }, {
                     enableHighAccuracy: true,
                     timeout: 1000 * 60,
@@ -95,6 +97,10 @@ function checkDeviceAndMoveNext() {
       unrecommended = true;
     }
   } else if (uaParser.getOS().name === "Android") {
+    if (Number(uaParser.getOS().version) < 5.0) {
+      unrecommended = true;
+    }
+
     if (uaParser.getBrowser().name === "Chrome"
             && Number(uaParser.getBrowser().version.split(".")[0]) >= 54) {
     } else {
@@ -105,7 +111,7 @@ function checkDeviceAndMoveNext() {
       unrecommended = true;
     }
 
-    if (uaParser.getBrowser().name === "Chrome" && Number(uaParser.getBrowser().version) > 54) {
+    if (uaParser.getBrowser().name === "Chrome" && Number(uaParser.getBrowser().version) >= 54) {
     } else if (uaParser.getBrowser().name.indexOf("Safari") != -1
             && Number(uaParser.getBrowser().version) >= 9.0) {
     } else {
@@ -120,15 +126,18 @@ function checkDeviceAndMoveNext() {
           + (uaParser.getOS() ? uaParser.getOS().name + " " + uaParser.getOS().version
                   : "unkown OS") + ")";
   if (unrecommended) {
-    $("#os-browser").html(
-            $('<div class="alert alert-warning">').html(
-                    '<span class="label label-warning">WARN</span> '
-                            + "OSまたはWebブラウザが推奨環境ではありません．正しく動作しない可能性があります．" + osAndBrowser));
-  } else {
     $("#os-browser")
             .html(
-                    $('<div class="alert alert-success">').html(
-                            '<span class="label label-success">SUCCESS</span> ' + "推奨環境です．"
-                                    + osAndBrowser));
+                    $('<div class="alert alert-warning">')
+                            .html(
+                                    '<span class="label label-warning">WARN</span> '
+                                            + osAndBrowser
+                                            + "は推奨ブラウザ/OSではないため，正しく動作しない可能性があります．"
+                                            + '<i class="glyphicon glyphicon-hand-right"></i> '
+                                            + '<a class="alert-link" href="troubleshooting.html#recommended-environment">推奨ブラウザ/OS</a> を見る．'));
+  } else {
+    $("#os-browser").html(
+            $('<div class="alert alert-success">').html(
+                    '<span class="label label-success">OK</span> ' + osAndBrowser + "は推奨環境です．"));
   }
 }
