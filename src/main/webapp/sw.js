d75d5fa6689abe5cab5c021ac96467992a99e092
Yuu NAKAJIMA <yuu.nkjm.pub@gmail.com>
2016-11-09 10:32:07 +0900

    キャッシュのクリーンを追加

--- src/main/webapp/sw.js
+++ src/main/webapp/sw.js
@@ -1,15 +1,27 @@
 var VERSION = 1;
 var CACHE_NAME = 'cache-v'+VERSION;
+var DOC_ROOT = location.href.replace("sw.js","")+"app/";
 
-function exchangeCitywalkUrl(url){
-  if(url.indexOf("magcruise-citywalk") !=-1 &&url.indexOf("?")!=-1){
-    return url.substring(0,url.indexOf("?"));
-  }else{
-    return url;
-  }
-}
+self.addEventListener('activate', function(evt) {
+  evt.waitUntil(
+    caches.keys().then(function(keys) {
+          var promises = [];
+          keys.forEach(function(cacheName) {
+              promises.push(caches.delete(cacheName));
+          });
+          return Promise.all(promises);
+    }));
+});
 
 self.addEventListener('fetch', (event) => {
+  function exchangeCitywalkUrl(url){
+    if(url.indexOf("magcruise-citywalk") !=-1 &&url.indexOf("?")!=-1){
+      return url.substring(0,url.indexOf("?"));
+    }else{
+      return url;
+    }
+  }
+
   var res = fetch(event.request).then((response) => {
       if (!response || response.status !== 200 || response.type !== 'basic') {
         return response;
